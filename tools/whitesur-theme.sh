c244e2dde240347c2c9adf030586ddb3300e5a7c
washburn24 <73243691+washburn24@users.noreply.github.com>
2024-08-31 22:02:35 -0500

    Create whitesur-theme.sh

---
+++ tools/whitesur-theme.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Make sure we're synced to GitHub repo...
+sudo zypper -n install git
+if [ ! -d $HOME/Documents/Software/Git/WhiteSur-gtk-theme ]; then
+    git clone https://github.com/vinceliuice/WhiteSur-gtk-theme $HOME/Documents/Software/Git/WhiteSur-gtk-theme
+else
+    git -C $HOME/Documents/Software/Git/WhiteSur-gtk-theme pull origin master
+fi
+
+# Install and configure
+mkdir $HOME/.themes
+$HOME/Documents/Software/Git/Whitesur-gtk-theme/install.sh
+$HOME/Documents/Software/Git/Whitesur-gtk-theme/install.sh -l -c Light
+$HOME/Documents/Software/Git/Whitesur-gtk-theme/tweaks.sh -F -c Light
+sudo flatpak override --filesystem=xdg-config/gtk-4.0
+sudo $HOME/Documents/Software/Git/Whitsur-gtk-theme/tweaks.sh -g -b $HOME/Pictures/Toscana.jpg
