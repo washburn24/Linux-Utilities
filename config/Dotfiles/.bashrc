9d74d2180ae4510d591c6ca6bbbb7d0012cfc97e
washburn24 <73243691+washburn24@users.noreply.github.com>
2026-04-01 18:47:34 -0500

    CachyOS Update

--- config/Dotfiles/.bashrc
+++ config/Dotfiles/.bashrc
@@ -1,27 +1,10 @@
-# .bashrc
+#
+# ~/.bashrc
+#
 
-# Source global definitions
-if [ -f /etc/bashrc ]; then
-    . /etc/bashrc
-fi
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
 
-# User specific environment
-if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
-    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
-fi
-export PATH
-
-# Uncomment the following line if you don't like systemctl's auto-paging feature:
-# export SYSTEMD_PAGER=
-
-# User specific aliases and functions
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-if [ -d ~/.bashrc.d ]; then
-    for rc in ~/.bashrc.d/*; do
-        if [ -f "$rc" ]; then
-            . "$rc"
-        fi
-    done
-fi
-unset rc
+PS1='[\u@\h \W]\$ '
--- tools/gnome-config.sh
+++ tools/gnome-config.sh
@@ -1,12 +1,22 @@
-#!/usr/bin/env bash
-
 # Function to check for command availability senses package tool to determine distro
 has_command() {
     command -v "$1" &> /dev/null
 }
 
+# Change sudo timeout so the whole script runs
+echo "Defaults timestamp_timeout=60" | sudo tee /etc/sudoers.d/timeout_settings
+sudo chmod 0440 /etc/sudoers.d/timeout_settings
+
 # Install some native applications by distro...
-if has_command apt; then
+if has_command pacman; then
+    echo "pacman package manager found, installing apps for Arch/CachyOS..."
+    sudo pacman -Syu --noconfirm
+    sudo pacman -Ru --noconfirm alacritty meld
+    sudo pacman -Syu --noconfirm geary flameshot flatpak diffuse gnome-menus      # Arch native repo
+    sudo pacman -Syu --noconfirm gnome-calendar gnome-contacts gnome-weather gnome-maps
+    sudo pacman -Syu --noconfirm apostrophe inkscape adw-gtk-theme python-pip octopi
+    paru -Syu --noconfirm joplin-desktop google-chrome
+elif has_command apt; then
     echo "apt package manager found, installing apps for Debian/Ubuntu..."
     sudo apt -y install git rsync gnome-tweaks flatpak gnome-software gnome-software-plugin-flatpak
     sudo apt -y install fastfetch gnome-contacts gnome-calendar geary flameshot   # Debian native repo
@@ -16,17 +26,13 @@ elif has_command dnf; then
 elif has_command zypper; then
     echo "zypper package manager found, installing apps for openSUSE..."
     sudo zypper -n install git neofetch geary flameshot inkscape gnome-tweaks     # openSUSE native repo
-elif has_command pacman; then
-    echo "pacman package manager found, installing apps for Manjaro..."
-    sudo pacman -Syu --noconfirm geary flameshot                                  # Arch native repo
 else
     echo "Warning: Linux distribution not detected for installation. Exiting to avoid harm."; exit 1
 fi
 
 # Install some flatpak applications...
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo flatpak -y install flathub io.github.shiftey.Desktop org.gnome.gitlab.somas.Apostrophe
-sudo flatpak -y install flathub zoom diffuse spotify org.vim.Vim com.discordapp.Discord
+flatpak -y install flathub io.github.shiftey.Desktop com.mattjakeman.ExtensionManager
 sudo flatpak override --filesystem=xdg-config/gtk-4.0
 
 # Clone some GitHub repos...
@@ -35,11 +41,6 @@ if [ ! -d $HOME/Documents/Software/Git/WhiteSur-icon-theme ]; then
 else
     git -C $HOME/Documents/Software/Git/WhiteSur-icon-theme pull origin master
 fi
-#if [ ! -d $HOME/Documents/Software/Git/WhiteSur-gtk-theme ]; then
-#    git clone https://github.com/vinceliuice/WhiteSur-gtk-theme $HOME/Documents/Software/Git/WhiteSur-gtk-theme
-#else
-#    git -C $HOME/Documents/Software/Git/WhiteSur-gtk-theme pull origin master
-#fi
 if [ ! -d $HOME/Documents/Linux-Utilities ]; then
     git clone https://github.com/washburn24/Linux-Utilities $HOME/Documents/Linux-Utilities
 else
@@ -47,25 +48,14 @@ else
 fi
 
 # Install some gtk, icon, and Vim themes...
-mkdir $HOME/.icons $HOME/.themes
+mkdir $HOME/.icons $HOME/.themes $HOME/.vim $HOME/.vim/colors
 $HOME/Documents/Software/Git/WhiteSur-icon-theme/install.sh -a -d $HOME/.icons -n WhiteSurAlt
 $HOME/Documents/Software/Git/WhiteSur-icon-theme/install.sh -d $HOME/.icons -n WhiteSurClean
 tar -C $HOME/.icons -xf $HOME/Documents/Linux-Utilities/config/Icons/WhiteSurCustom.tar.xz
 rsync -a --ignore-existing $HOME/.icons/WhiteSurClean/* $HOME/.icons/WhiteSur/  # Sync only new icons to not overwrite custom changes
-sudo cp $HOME/Documents/Linux-Utilities/config/Themes/codedark.vim /usr/share/vim/vim91/colors/
-
-# Install and configure WhiteSur theme for Mac style controls...
-#$HOME/Documents/Software/Git/WhiteSur-gtk-theme/install.sh
-#$HOME/Documents/Software/Git/WhiteSur-gtk-theme/install.sh -l -c light -o solid
-#$HOME/Documents/Software/Git/WhiteSur-gtk-theme/tweaks.sh -F -c light
-#$HOME/Documents/Software/Git/WhiteSur-gtk-theme/install.sh
+sudo cp $HOME/Documents/Linux-Utilities/config/Themes/codedark.vim $HOME/.vim/colors/
 
 # Make some custom changes that are only relevant to me...
-#if [ ! -d /usr/share/gnome-shell/theme/Yaru-blue ]; then
-#    sudo mkdir /usr/share/gnome-shell/theme
-#    sudo mkdir /usr/share/gnome-shell/theme/Yaru-blue
-#    sudo cp $HOME/Documents/Linux-Utilities/config/Themes/Yaru-blue-Ubuntu2404.css /usr/share/gnome-shell/theme/Yaru-blue/gnome-shell.css
-#fi
 #rsync -a --ignore-existing $HOME/Documents/Linux-Utilities/config/Themes/MacLight/* $HOME/.themes/MacLight/
 rsync -a --ignore-existing $HOME/Documents/Linux-Utilities/config/Themes/MacDark/* $HOME/.themes/MacDark/
 sudo cp -f $HOME/Documents/Linux-Utilities/audio/alsa-base.conf /etc/modprobe.d/  # Bug fix for audio on Lenovo Yoga 9
@@ -73,11 +63,6 @@ if [ ! -f $HOME/.vimrc ]; then  # Copy new dotfiles if they don't exist or if th
     cp $HOME/Documents/Linux-Utilities/config/Dotfiles/.vimrc $HOME
 else
     rsync -a $HOME/.vimrc $HOME/Documents/Linux-Utilities/config/Dotfiles/
-fi
-if [ ! -f $HOME/.bashrc ]; then
-    cp $HOME/Documents/Linux-Utilities/config/Dotfiles/.bashrc $HOME
-else
-    rsync -a $HOME/.bashrc $HOME/Documents/Linux-Utilities/config/Dotfiles/
 fi
 
 # Gnome general settings adjustments...
@@ -88,9 +73,9 @@ gsettings set org.gnome.desktop.interface show-battery-percentage 'true'
 
 # Command line control of gnome-tweaks; this sets icons, shell, legacy app themes, and title bar formatting...
 # Syntax for gsettings can be deciphered using 'dconf watch /' while monitoring settings adjustments in GUI tools
-gsettings set org.gnome.desktop.interface icon-theme 'WhiteSur'
-dconf write /org/gnome/shell/extensions/user-theme/name 'MacDark'
-gsettings set org.gnome.desktop.interface gtk-theme 'MacDark'
+dconf write /org/gnome/desktop/interface/icon-theme "'WhiteSur'"
+dconf write /org/gnome/shell/extensions/user-theme/name "'MacDark'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'adw-gtk3'"
 #gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:appmenu'
 
 # Controlling Just Perfection from the command line (move clock and notifications to the right)...
@@ -103,21 +88,3 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name Flameshot
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command ~/Documents/Linux-Utilities/tools/flameshot-workaround.sh
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding Print
-
-# Install Google Chrome but delete any old versions hanging around...
-if [ -f google-chrome-stable_current_x86_64.rpm ] || [ -f google-chrome-stable_current_amd64.deb ]; then
-    rm -f google-chrome-stable_current_*
-fi
-if has_command zypper; then
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-#    sudo zypper -n install ./google-chrome-stable_current_x86_64.rpm
-elif has_command dnf; then
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-    sudo dnf -y install ./google-chrome-stable_current_x86_64.rpm
-elif has_command apt; then
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo apt -y install ./google-chrome-stable_current_amd64.deb
-#elif has_command pamac; then
-#    sudo pamac build --no-confirm google-chrome
-fi
-rm -f google-chrome-stable_current_*
